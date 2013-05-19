dafd7493988f537be2599c746857296a08dd4b01
Yu Chen Hou <houyuchen66@gmail.com>
2013-05-19 05:04:20 -0400

    Initial commit

---
+++ ToneMatrix/src/main/java/com/icechen1/tonematrix/ControlFragment.java
@@ -0,0 +1,13 @@
+package com.icechen1.tonematrix;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Icechen1 on 19/05/13.
+ */
+public class ControlFragment extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ ToneMatrix/src/main/java/com/icechen1/tonematrix/GridFragment.java
@@ -0,0 +1,7 @@
+package com.icechen1.tonematrix;
+
+/**
+ * Created by Icechen1 on 19/05/13.
+ */
+public class GridFragment {
+}
